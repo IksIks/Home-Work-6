8b70d96557e210ea05bce3fb6e49e9e18d408270
iksiks <iksiks30@gmail.com>
2020-12-22 00:41:39 +0300

    В GONv.2 добавлен основной код.

--- GroupsOfNumberVersion2/Program.cs
+++ GroupsOfNumberVersion2/Program.cs
@@ -5,11 +5,38 @@ namespace GroupsOfNumberVersion2
 {
     class Program
     {
+        static int ExpNumber(int N)
+        {
+            return (int)Math.Pow(2, N);           
+        }
         static void Main(string[] args)
         {
-            File.WriteAllText(@"number.txt", "50");
+            File.WriteAllText(@"number.txt", "100");
             int numberFromFile = int.Parse(File.ReadAllText(@"number.txt"));
             int M = (int)Math.Log(numberFromFile, 2) + 1;
+            int minExp = 1;
+            int maxExp = ExpNumber(0);
+            int count = 0;
+            DateTime start = DateTime.Now;
+            using StreamWriter numbersWriter = new StreamWriter("test.txt");
+            {
+                for (int i = 1; i <= numberFromFile; i++)
+                {
+                    if (i >= minExp && i < maxExp)                        
+                        numbersWriter.Write($"{i}  ", true);
+                    else
+                    {
+                        numbersWriter.WriteLine();
+                        numbersWriter.Write($"{i}  ", true);
+                        count++;
+                        minExp = maxExp;
+                        maxExp = ExpNumber(count);
+                    }
+                }
+            }
+            TimeSpan ts = DateTime.Now.Subtract(start);
+            Console.WriteLine($"Время: {ts.Minutes} минут, {ts.Seconds} секуннд {ts.Milliseconds} миллисекунд");
+            Console.ReadKey();
         }
     }
 }
