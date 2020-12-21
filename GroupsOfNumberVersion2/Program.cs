90d63ccec9b24dde2cb53d461aa9d24855966a0e
iksiks <iksiks30@gmail.com>
2020-12-22 01:02:06 +0300

    Доработка

--- GroupsOfNumberVersion2/Program.cs
+++ GroupsOfNumberVersion2/Program.cs
@@ -11,12 +11,12 @@ namespace GroupsOfNumberVersion2
         }
         static void Main(string[] args)
         {
-            File.WriteAllText(@"number.txt", "100");
+            File.WriteAllText(@"number.txt", "5000");
             int numberFromFile = int.Parse(File.ReadAllText(@"number.txt"));
-            int M = (int)Math.Log(numberFromFile, 2) + 1;
+            //int M = (int)Math.Log(numberFromFile, 2) + 1;
             int minExp = 1;
-            int maxExp = ExpNumber(0);
-            int count = 0;
+            int maxExp = ExpNumber(1);
+            int count = 1;
             DateTime start = DateTime.Now;
             using StreamWriter numbersWriter = new StreamWriter("test.txt");
             {
